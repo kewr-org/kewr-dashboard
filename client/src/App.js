0f36b37c3d3f36eee04af54fda8f24313a7f81fe
Pabo Bernando <pabobernando69@gmail.com>
2022-08-15 21:36:16 +0700

    change UI

--- client/src/App.js
+++ client/src/App.js
@@ -16,38 +16,7 @@ const [categories, setCategories] = useState();
 const [categoriesIris, setCategoriesIris] = useState();
 const [categoriesCrescent, setCategoriesCrescent] = useState();
 const [address, setAddress] = useState('');
-const ibcListsOsmosis = [
-  {
-    "denom": "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2",
-    "alias": "ATOM (ATOM/Channel-0"
-    },
-    {
-    "denom": "ibc/6AE98883D4D5D5FF9E50D7130F1305DA2FFA0C652D1DD9C123657C6B4EB2DF8A",
-    "alias": "EVMOS (EVMOS/Channel-204)"
-    },
-    {
-    "denom": "ibc/0EF15DF2F02480ADE0BB6E85D9EBB5DAEA2836D3860E9F97F9AADE4F57A31AA0",
-    "alias": "LUNC (terra-luna/Channel-72)"
-    },
-    {
-    "denom": "ibc/FE2CD1E6828EC0FAB8AF39BAC45BC25B965BA67CCBC50C13A14BD610B0D1E2C4",
-    "alias": "BOOT (BOSTROM/Channel-95"
-    },
-    {
-    "denom": "ibc/785AFEC6B3741100D15E7AF01374E3C4C36F24888E96479B1C33F5C71F364EF9",
-    "alias": "LUNA (Terra 2.0/Channel-251)"
-    },
-    {
-    "denom": "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858",
-    "alias": "USDC (Axelar/channel-208)"
-    },
-    {
-    "denom": "uosmo",
-    "alias": "OSMO"
-    }
-];
 
- 
 //  keplr
  async function handleClick() {
   const chainId = "cosmoshub-1"
@@ -57,7 +26,6 @@ const ibcListsOsmosis = [
   setAddress(accounts[0].address)
  }
 // akhir keplr
-
 
 useEffect(() => {
   retrieveCategories();
@@ -93,7 +61,7 @@ useEffect(() => {
         }catch(error){
           console.log(error)
         }
-      }
+      };
 
   return (
     <div className="App"> 
@@ -107,26 +75,35 @@ useEffect(() => {
           <h4 className='text-white'></h4>
           </div>
         </div>
-        <div className='row text-white text-center mt-5'>
-          <div className='col-4'>
+        <div className='row text-white text-center d-flex justify-content-center mt-5'>
+          <div className='col-5'>
             <h4 className='border border-info'>Osmosis</h4>
-            <p>{JSON.stringify(categories)}</p>
-            {/* {ibcListsOsmosis.map((ibcListsOsmosis,index) => (
-              <ul>
-                  <li>{ibcListsOsmosis.denom}</li>
-                  <li>{ibcListsOsmosis.alias}</li>
-              </ul>
-            ))} */}
+            {/* <p>{JSON.stringify(categories)}</p> */}
+            <ul>
+              {categories.map((data,index) => (
+                <p >- {data.denom} <br />{data.amount} </p>
+              ))}
+          </ul>
           </div>
-          <div className='col-4'>
+          <div className='col-5'>
             <h4 className='border border-info'>Iris</h4>
-            <p>{JSON.stringify(categoriesIris)}</p>
-          </div>
-          <div className='col-4'>
-            <h4 className='border border-info'>Crescent</h4>
-            <p>{JSON.stringify(categoriesCrescent)}</p>
+            <ul>
+              {categoriesIris.map((data,index) => (
+                <p>- {data.denom}<br />{data.amount} </p>
+              ))}
+          </ul>
           </div>
         </div>
+        <row className='text-white text-center d-flex justify-content-center mt-5'>
+          <div className='col-6'>
+            <h4 className='border border-info'>Crescent</h4>
+            <ul>
+              {categoriesCrescent.map((data,index) => (
+                <p > - {data.denom}<br />{data.amount} </p>
+              ))}
+          </ul>
+          </div>
+        </row>
         <div className='row text-white text-center mt-5 '>
           <div className='col-2'>
             
