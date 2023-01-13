6bff43c8254401fd0625ddc643b8f1960733e3c1
Pabo Bernando <pabobernando69@gmail.com>
2023-01-14 03:09:46 +0700

    check price

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import './App.css' ;
-import Icon from './tangan.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useState,useEffect} from 'react';
-import { Button } from 'react-bootstrap';
-import { FaBeer,FaSort,FaAngleDoubleRight,FaExchangeAlt } from 'react-icons/fa';
 import Navbar from './components/navbar';
 import Balance from './components/balance';
 import Swap from './components/swap';
 import Ibc from './components/ibc';
 import Tes from './components/tesswap';
-import PriceCheckCrescent from './components/PriceCheckCrescent'
 
 function App() {
   return (
@@ -23,11 +19,6 @@ function App() {
         <Ibc />
         <Tes />
       </div>
-      <PriceCheckCrescent 
-url1="https://mainnet.crescent.network:1317/crescent/liquidity/v1beta1/pools/3"
-url2="https://mainnet.crescent.network:1317/crescent/liquidity/v1beta1/pools/10" 
-url3="https://mainnet.crescent.network:1317/crescent/liquidity/v1beta1/pools/19"   
-/>
     </div>
   );
 }
--- client/src/components/tesswap.js
+++ client/src/components/tesswap.js
@@ -1,44 +1,73 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 
 function Tesswap() {
-    const [data, setData] = useState([])
-    const [kali, setKali] = useState()
-    const [hasil, setHasil] = useState()
-    const urlbcre = "https://mainnet.crescent.network:1317/crescent/liquidity/v1beta1/pools/3"
-    const urlgweth = "https://mainnet.crescent.network:1317/crescent/liquidity/v1beta1/pools/10" 
-    const urlUsd = "https://mainnet.crescent.network:1317/crescent/liquidity/v1beta1/pools/19"   
+const [dataBcre, setDataBcre] = useState()
+const [dataGrav, setDataGrav] = useState()
+const [dataUsd, setDataUsd] = useState()
+const [isiInput, setIsiIInput] = useState()
+const [hasilBcre, setHasilBcre] = useState()
+const [hasilGrav, setHasilGrav] = useState()
+const [hasilUsd, setHasilUsd] = useState()
+const urlBcre = "https://mainnet.crescent.network:1317/crescent/liquidity/v1beta1/pools/3"
+const urlGrav = "https://mainnet.crescent.network:1317/crescent/liquidity/v1beta1/pools/10"
+const urlUsd = "https://mainnet.crescent.network:1317/crescent/liquidity/v1beta1/pools/19"
+const urlOsmoAtom = "https://osmosis-api.polkachu.com/osmosis/gamm/v1beta1/pools/1/prices?base_asset_denom=uosmo&quote_asset_denom=ibc%2F27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"
+
+useEffect(() => {
+  axios.get(urlBcre)
+  .then(response => {
+      setDataBcre(response.data.pool.price)
+  })
+  .catch(error => {
+      console.log("Error dari urlCre :",error)
+  });
+
+axios.get(urlGrav)
+  .then(response => {
+      setDataGrav(response.data.pool.price)
+      console.log(dataGrav)
+  })
+  .catch(error => {
+      console.log("Error dari urlGrav :",error)
+  });
+
+axios.get(urlUsd)
+  .then(response => {
+      setDataUsd(response.data.pool.price)
+  })
+  .catch(error => {
+      console.log("Error dari urlUsd :",error)
+  });
+})
 
 
-    useEffect(() => {
-        axios.get(urlUsd)
-          .then(response => {
-            setData(response.data.pool.price);
-            console.log(data)
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }, []);
+const addPrice = (event) => {
+  console.log(dataBcre)
+  event.preventDefault()
+  setHasilBcre(dataBcre * isiInput)
+  setHasilGrav(isiInput * dataBcre / dataGrav)
+  setHasilUsd(isiInput * dataUsd)
+  console.log(hasilGrav)
+}
 
-      const handleChange = (e) => {
-        console.log("tes input")
-        setKali(e.target.value)
-        if(data !== null && data !==undefined){
-            setHasil(kali * data )
-        }
-      }
-
-      const handleSwap = () => {
-        console.log("kirim swap")
-      }
+const handleChange = (event) => {
+  setIsiIInput(event.target.value)
+  setHasilBcre("")
+  setHasilGrav("")
+  setHasilUsd("")
+}
 
   return (
-    <div>
-        <h1 className='text-white'>Swap</h1>
-        <input type="number" onChange={handleChange} value={kali} placeholder="ATOM"></input>
-        <h3 className='text-white'>bCre : {hasil}</h3>
-        <button className='btn btn-primary' onClick={handleSwap}>SWAP</button>
+    <div className='text-white'>
+      <h1>Price Check Crescent</h1>
+      <form onSubmit={addPrice}>
+        <input onChange={handleChange}></input>
+        <button type='submit' className='btn btn-primary'>Click Check Price</button>
+      </form>
+      <h1>BCRE : {hasilBcre}</h1>
+      <h1>GRAV : {hasilGrav}</h1>
+      <h1>USDC AXL : {hasilUsd}</h1>
     </div>
   )
 }
