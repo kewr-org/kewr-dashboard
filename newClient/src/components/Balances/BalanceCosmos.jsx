cd29cbebb84cbbde6eb43f69fdfcc4defe87f87a
Pabo Bernando <pabobernando69@gmail.com>
2023-02-03 05:25:01 +0700

    cors

--- newClient/src/components/Balances/BalanceCosmos.jsx
+++ newClient/src/components/Balances/BalanceCosmos.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from "react";
-import LogoAtom from '../../assets/cosmos.svg';
 import { SigningStargateClient } from "@cosmjs/stargate";
 
 const BalanceCosmos = () => {
@@ -15,15 +14,15 @@ const BalanceCosmos = () => {
         const accounts = await offlineSigner.getAccounts();
         const Address = accounts[0].address;
         const client = await SigningStargateClient.connectWithSigner(
-          "https://rpc-cosmoshub.blockapsis.com",
+          "https://rpc.cosmos.dragonstake.io",
           offlineSigner
         );
-      let allBalances;
+      // let allBalances;
       await client.getAllBalances(Address)
       .then(data => {
-      allBalances = data;
-      setAllBalances(data);
-      console.log(data);
+      // allBalances = data;
+      const dataString = JSON.stringify(data)
+      setAllBalances(dataString);
   });
       
       }
@@ -35,23 +34,9 @@ const BalanceCosmos = () => {
     <div className="w-full md:w-1/2 xl:w-1/3 p-6">
       <div className="bg-grey border border-cyan-300 border-b-4 rounded-lg shadow-xl p-5">
         <div className="flex flex-row items-center">
-          <div className="flex-shrink pr-4">
-            <div className="rounded-full  bg-black">
-              <img src={LogoAtom} width="70" className="rounded-full" />
-            </div>
-          </div>
           <div className="flex-1 text-right md:text-center">
-          <div>
-          <ul>
-      {AllBalances && AllBalances.map(balance => (
-        <li key={balance.denom}>
-          {balance.denom}: {balance.amount}
-        </li>
-      ))}
-    </ul>
-          </div>
-            <h2 className="font-bold text-white">COSMOS SU</h2>
-            <p className="font-bold text-3xl text-white">tes123</p>
+            <h2 className="font-bold text-white">COSMOS HUB</h2>
+            <p className="font-bold text-1xl text-white">{AllBalances}</p>
           </div>
         </div>
       </div>
