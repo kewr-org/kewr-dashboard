1f99558cae0b073430168048ea2298f53865a61d
Hendri Sulistya <id.hendrisulistya@gmail.com>
2023-02-05 12:06:18 +0700

    SuggestChain feature progress

--- newClient/src/Pages/PageThree.jsx
+++ newClient/src/Pages/PageThree.jsx
@@ -1,24 +1,9 @@
 import React from "react";
+import ChainAdd from "../components/ChainAdd";
 
 function PageThree() {
   return (
-    <>
-  <div className="flex flex-row h-64 justify-around">
-  <div className="flex flex-col justify-around justify-items-center">
-  <button class="bg-teal-200 hover:bg-teal-400 text-gray-800 font-semibold py-2 px-4 border  rounded shadow">✚ | Canto </button>
-  <button class="bg-teal-200 hover:bg-teal-400 text-gray-800 font-semibold py-2 px-4 border  rounded shadow">✚ | Echelon </button>
-  </div>
-  <div className="flex flex-col justify-around justify-items-center">
-  <button class="bg-teal-200 hover:bg-teal-400 text-gray-800 font-semibold py-2 px-4 border  rounded shadow">✚ | Tera Classic </button>
-  <button class="bg-teal-200 hover:bg-teal-400 text-gray-800 font-semibold py-2 px-4 border  rounded shadow">✚ | Acre Chain</button>
-  </div>
-  <div className="flex flex-col justify-around justify-items-center">
-  <button class="bg-teal-200 hover:bg-teal-400 text-gray-800 font-semibold py-2 px-4 border  rounded shadow">✚ | Cosmoshub Testnet </button>
-  <button class="bg-teal-200 hover:bg-teal-400 text-gray-800 font-semibold py-2 px-4 border  rounded shadow">✚ | Gnoland</button>
-  </div>
-
-  </div>
-  </>
+    <ChainAdd />
   );
 }
 
